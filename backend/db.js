b536da3ebf52be54a64cbb24f0ebda3308fd03f3
LeonelGonzalez1105 <Leonelgh1@hotmail.com>
2026-04-11 22:34:24 -0600

    version 1.11
    pongo "1.11" porque lo que se modifico fue la comunicacion con la base de datos ya que este se agrego a un servicio de nube para que podamos usar todos esta base de datos

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,22 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Leonelhalo2003.',
-    database: 'almacen_electronica'
-
+  host: 'gateway01.us-east-1.prod.aws.tidbcloud.com',
+  port: 4000,
+  user: '4HBSXKLUs96dsK7.root',
+  password: '5CwVuuw7mWbCGuu0',
+  database: 'almacen_electronica',
+  ssl: {
+      rejectUnauthorized: true
+  }
 });
 
-db.connect((err) =>{
-    if (err) {
-        console.error('Error de conexion:' , err);
-        return;
-    }
-    console.log('¡Conectado a la base de datos de Almacen!');
-
+db.connect((err) => {
+  if (err) {
+    console.error('Error de conexion:', err);
+    return;
+  }
+  console.log('¡Conectado a la base de datos en la NUBE!');
 });
 
 module.exports = db;
